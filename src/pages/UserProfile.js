eea116f1eb1f64b3c1b825fddd5a3aaeff6222f9
Arielwz <arieldududu@gmail.com>
2023-03-16 15:07:07 -0700

    Added UserProfile

---
+++ src/pages/UserProfile.js
@@ -0,0 +1,157 @@
+import styled, {createGlobalStyle} from "styled-components";
+import {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {publicRequest} from "../requestMethods";
+import {Close, ArrowBack} from '@material-ui/icons';
+import {deleteOrderThunk} from "../redux/orderRedux";
+import {Link} from "react-router-dom";
+import {useNavigate} from "react-router";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+      background: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255,255,255,0.5)), #61dafb;
+    }
+`
+
+const Container = styled.div`
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+`
+
+const Wrapper = styled.div`
+    width: 50%;
+    padding: 20px;
+    background-color: white;
+`
+
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+    font-weight: bold;
+    text-align: center;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`
+
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+    margin-bottom: 10px;
+    &:disabled {
+    color: green;
+    cursor: not-allowed;
+    }
+`
+
+const Error = styled.span`
+    color: red;
+`
+
+const Label = styled.label`
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0px;
+    padding: 10px;
+`
+
+const OrderDiv = styled.div`
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+`
+
+const CloseDiv = styled.div`
+    margin-top: 15px;
+    width: 5%;
+    align-items: flex-end;
+    cursor: pointer;
+`
+
+const OrderContainer = styled.div`
+    display: flex;
+`
+
+const OrderTitle = styled.h1`
+    margin-top: 30px;
+    font-size: 24px;
+    font-weight: 300;
+    font-weight: bold;
+    text-align: center;
+`
+
+const Div1 = styled.div`
+`
+
+const UserProfile = () => {
+    const user = useSelector((state) => state.user.currentUser);
+
+    const id = user._id;
+    const[orders, setOrders] = useState(null);
+    useEffect(() => {
+        const getOrders = async () => {
+            try {
+                const res = await publicRequest.get("/orders/find/" + id);
+                setOrders(res.data);
+                // console.log(res.data);
+            } catch (err){
+                // console.log(err);
+            }
+        };
+        getOrders();
+    });
+
+    const dispatch = useDispatch();
+    const goBack = useNavigate();
+    const deleteOrderHandler = (orderId) => {
+        dispatch(deleteOrderThunk(orderId));
+    }
+
+    return (
+        <div>
+            <GlobalStyle/>
+            <Container>
+                <Wrapper>
+                    <Div1>
+                        <ArrowBack onClick={() => goBack(-1)}/>
+                        <Title>Profile: {user.firstname} {user.lastname}</Title>
+                    </Div1>
+
+                    <Form>
+                        <Label>Username: {user.username}</Label>
+                        <Label>Email: {user.email}</Label>
+                        <Link to="/editProfile">
+                            <Button>Edit Profile</Button>
+                        </Link>
+
+                        <OrderTitle>Orders</OrderTitle>
+                        {orders && orders.map((order) => (
+                            <OrderContainer>
+                                <OrderDiv>
+                                    <Link style={{cursor: "pointer"}} to={`/order/${order._id}`}>
+                                        <Label>Order id: {order._id}</Label>
+                                    </Link>
+                                    <Label>Order Amount: ${order.amount}</Label>
+                                </OrderDiv>
+                                <CloseDiv>
+                                    <Close onClick={() => {deleteOrderHandler(order._id);}}/>
+                                </CloseDiv>
+                            </OrderContainer>
+                        )) }
+
+                    </Form>
+                </Wrapper>
+            </Container>
+        </div>
+    )
+}
+
+export default UserProfile
