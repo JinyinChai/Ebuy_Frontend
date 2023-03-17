709af353bd12428c6e9dfc08becb471beb1af00f
Jinyin Chai <cjy990324>
2023-03-17 13:20:11 -0700

    Initial files

---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const categories = [
+    {
+        id: 1,
+        img: "https://www.billbazar.com.bd/public/uploads/all/1XdNnCsHg4iAAFIV4oXJCV1hPs7WSld1lvuOLHfD.webp",
+        title: "See All Products",
+        cat: "women",
+    },
+
+];
